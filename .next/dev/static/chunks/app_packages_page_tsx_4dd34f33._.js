47d90b88753e05e1326cb43fb9ff0bba76da36a4
Asad Atik <asadatik1995@gmail.com>
2026-03-16 23:11:24 +0600

    chore: remove .next build files

--- .next/dev/static/chunks/app_packages_page_tsx_4dd34f33._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_682775f4._.js",
-  "static/chunks/_297e1957._.js"
-],
-    source: "dynamic"
-});
